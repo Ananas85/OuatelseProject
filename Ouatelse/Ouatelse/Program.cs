abd843963244f1327a903076729015c35cb0bc77
anaelChardan <anael.chardan@gmail.com>
2015-02-02 15:06:49 +0100

    Just start the page to see

--- Ouatelse/Ouatelse/Program.cs
+++ Ouatelse/Ouatelse/Program.cs
@@ -16,7 +16,7 @@ namespace Ouatelse
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new InitialPage());
         }
     }
 }
