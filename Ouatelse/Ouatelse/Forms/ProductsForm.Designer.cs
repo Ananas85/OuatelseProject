b0eef024ac3cb167cd0d8993719a3f4507bedd77
SSVAV <velezsarain@gmail.com>
2015-02-09 17:26:23 +0100

    no message

---
+++ Ouatelse/Ouatelse/Forms/ProductsForm.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Ouatelse.Forms
+{
+    partial class ProductsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView_employees = new System.Windows.Forms.ListView();
+            this.reference_column = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lastname_column = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.firstname_column = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.mail_column = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.role_column = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.store_column = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.clientsBtn = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView_employees
+            // 
+            this.listView_employees.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView_employees.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.reference_column,
+            this.lastname_column,
+            this.firstname_column,
+            this.mail_column,
+            this.role_column,
+            this.store_column});
+            this.listView_employees.Location = new System.Drawing.Point(11, 214);
+            this.listView_employees.Margin = new System.Windows.Forms.Padding(2);
+            this.listView_employees.Name = "listView_employees";
+            this.listView_employees.Size = new System.Drawing.Size(920, 273);
+            this.listView_employees.TabIndex = 29;
+            this.listView_employees.UseCompatibleStateImageBehavior = false;
+            this.listView_employees.View = System.Windows.Forms.View.Details;
+            // 
+            // reference_column
+            // 
+            this.reference_column.Text = "Référence";
+            this.reference_column.Width = 118;
+            // 
+            // lastname_column
+            // 
+            this.lastname_column.Text = "Nom";
+            this.lastname_column.Width = 204;
+            // 
+            // firstname_column
+            // 
+            this.firstname_column.Text = "Prénom";
+            this.firstname_column.Width = 246;
+            // 
+            // mail_column
+            // 
+            this.mail_column.Text = "Adresse mail";
+            this.mail_column.Width = 306;
+            // 
+            // role_column
+            // 
+            this.role_column.Text = "Rôle";
+            this.role_column.Width = 239;
+            // 
+            // store_column
+            // 
+            this.store_column.Text = "Magasin";
+            this.store_column.Width = 232;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(116, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 36);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "Gestion du stock";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Ouatelse.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(90, 65);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 27;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.IndianRed;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(392, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(184, 41);
+            this.button2.TabIndex = 26;
+            this.button2.Text = "Supprimer un produit";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(202, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 41);
+            this.button1.TabIndex = 25;
+            this.button1.Text = "Modifier un produit";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // clientsBtn
+            // 
+            this.clientsBtn.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.clientsBtn.FlatAppearance.BorderSize = 0;
+            this.clientsBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.clientsBtn.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clientsBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.clientsBtn.Location = new System.Drawing.Point(12, 100);
+            this.clientsBtn.Name = "clientsBtn";
+            this.clientsBtn.Size = new System.Drawing.Size(184, 41);
+            this.clientsBtn.TabIndex = 24;
+            this.clientsBtn.Text = "Nouveau produit";
+            this.clientsBtn.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Location = new System.Drawing.Point(106, 147);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(184, 41);
+            this.button3.TabIndex = 30;
+            this.button3.Text = "Réapprovisionner un produit";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button4.Location = new System.Drawing.Point(296, 147);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(184, 41);
+            this.button4.TabIndex = 31;
+            this.button4.Text = "Déstocker un produit";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // ProductsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(846, 351);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView_employees);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.clientsBtn);
+            this.Name = "ProductsForm";
+            this.Text = "ProductsForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView_employees;
+        private System.Windows.Forms.ColumnHeader reference_column;
+        private System.Windows.Forms.ColumnHeader lastname_column;
+        private System.Windows.Forms.ColumnHeader firstname_column;
+        private System.Windows.Forms.ColumnHeader mail_column;
+        private System.Windows.Forms.ColumnHeader role_column;
+        private System.Windows.Forms.ColumnHeader store_column;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button clientsBtn;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+
+    }
+}
