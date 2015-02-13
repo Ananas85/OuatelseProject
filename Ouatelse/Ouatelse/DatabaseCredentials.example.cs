a708bd249b558c8c0b3d9d096b4112104fb540a5
corentinmb <corentin.moreau2@gmail.com>
2015-02-13 21:51:20 +0100

    I missclicked no scope 360 this file. Re-added.

---
+++ Ouatelse/Ouatelse/DatabaseCredentials.example.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ouatelse
+{
+    public static class DatabaseCredentials
+    {
+        public static string Host { get { return "chardan.net"; } }
+        public static string Username { get { return "drte"; } }
+        public static string Password { get { return ""; } }
+        public static string DatabaseName { get { return "Ouatelse"; } }
+        public static string Port { get { return "1664"; } }
+    }
+}
+
