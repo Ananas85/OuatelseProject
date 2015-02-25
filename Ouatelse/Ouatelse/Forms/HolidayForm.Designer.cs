d4415e86bf473d601633766576181c2d30c9f364
Ananas-Mac <anael.chardan@etu.u-bordeaux.fr>
2015-02-25 19:15:23 +0100

    Try to commit :p

---
+++ Ouatelse/Ouatelse/Forms/HolidayForm.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Ouatelse.Forms
+{
+    partial class HolidayForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.holidays = new System.Windows.Forms.DataGridView();
+            this.one = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.two = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.three = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.four = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.five = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.six = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.seven = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.eight = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nine = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ten = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.onze = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.douze = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.treize = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.quatorze = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.quinze = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.seize = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dixsept = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dixhuit = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dixneuf = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.vingt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.vingtun = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.vingtdeux = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.vingttrois = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.vingtquatre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.vingtcinq = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.vingtsix = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.vingtsept = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.vingthuit = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.vingtneuf = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.trente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.trenteun = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.holidays)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // holidays
+            // 
+            this.holidays.AllowUserToAddRows = false;
+            this.holidays.AllowUserToDeleteRows = false;
+            this.holidays.AllowUserToResizeColumns = false;
+            this.holidays.AllowUserToResizeRows = false;
+            this.holidays.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.holidays.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.one,
+            this.two,
+            this.three,
+            this.four,
+            this.five,
+            this.six,
+            this.seven,
+            this.eight,
+            this.nine,
+            this.ten,
+            this.onze,
+            this.douze,
+            this.treize,
+            this.quatorze,
+            this.quinze,
+            this.seize,
+            this.dixsept,
+            this.dixhuit,
+            this.dixneuf,
+            this.vingt,
+            this.vingtun,
+            this.vingtdeux,
+            this.vingttrois,
+            this.vingtquatre,
+            this.vingtcinq,
+            this.vingtsix,
+            this.vingtsept,
+            this.vingthuit,
+            this.vingtneuf,
+            this.trente,
+            this.trenteun});
+            this.holidays.Location = new System.Drawing.Point(12, 61);
+            this.holidays.Name = "holidays";
+            this.holidays.Size = new System.Drawing.Size(1058, 382);
+            this.holidays.TabIndex = 0;
+            // 
+            // one
+            // 
+            this.one.HeaderText = "1";
+            this.one.Name = "one";
+            // 
+            // two
+            // 
+            this.two.HeaderText = "2";
+            this.two.Name = "two";
+            // 
+            // three
+            // 
+            this.three.HeaderText = "3";
+            this.three.Name = "three";
+            // 
+            // four
+            // 
+            this.four.HeaderText = "4";
+            this.four.Name = "four";
+            // 
+            // five
+            // 
+            this.five.HeaderText = "5";
+            this.five.Name = "five";
+            // 
+            // six
+            // 
+            this.six.HeaderText = "6";
+            this.six.Name = "six";
+            // 
+            // seven
+            // 
+            this.seven.HeaderText = "7";
+            this.seven.Name = "seven";
+            // 
+            // eight
+            // 
+            this.eight.HeaderText = "8";
+            this.eight.Name = "eight";
+            // 
+            // nine
+            // 
+            this.nine.HeaderText = "9";
+            this.nine.Name = "nine";
+            // 
+            // ten
+            // 
+            this.ten.HeaderText = "10";
+            this.ten.Name = "ten";
+            // 
+            // onze
+            // 
+            this.onze.HeaderText = "11";
+            this.onze.Name = "onze";
+            // 
+            // douze
+            // 
+            this.douze.HeaderText = "12";
+            this.douze.Name = "douze";
+            // 
+            // treize
+            // 
+            this.treize.HeaderText = "13";
+            this.treize.Name = "treize";
+            // 
+            // quatorze
+            // 
+            this.quatorze.HeaderText = "14";
+            this.quatorze.Name = "quatorze";
+            // 
+            // quinze
+            // 
+            this.quinze.HeaderText = "15";
+            this.quinze.Name = "quinze";
+            // 
+            // seize
+            // 
+            this.seize.HeaderText = "16";
+            this.seize.Name = "seize";
+            // 
+            // dixsept
+            // 
+            this.dixsept.HeaderText = "17";
+            this.dixsept.Name = "dixsept";
+            // 
+            // dixhuit
+            // 
+            this.dixhuit.HeaderText = "18";
+            this.dixhuit.Name = "dixhuit";
+            // 
+            // dixneuf
+            // 
+            this.dixneuf.HeaderText = "19";
+            this.dixneuf.Name = "dixneuf";
+            // 
+            // vingt
+            // 
+            this.vingt.HeaderText = "20";
+            this.vingt.Name = "vingt";
+            // 
+            // vingtun
+            // 
+            this.vingtun.HeaderText = "21";
+            this.vingtun.Name = "vingtun";
+            // 
+            // vingtdeux
+            // 
+            this.vingtdeux.HeaderText = "22";
+            this.vingtdeux.Name = "vingtdeux";
+            // 
+            // vingttrois
+            // 
+            this.vingttrois.HeaderText = "23";
+            this.vingttrois.Name = "vingttrois";
+            // 
+            // vingtquatre
+            // 
+            this.vingtquatre.HeaderText = "24";
+            this.vingtquatre.Name = "vingtquatre";
+            // 
+            // vingtcinq
+            // 
+            this.vingtcinq.HeaderText = "25";
+            this.vingtcinq.Name = "vingtcinq";
+            // 
+            // vingtsix
+            // 
+            this.vingtsix.HeaderText = "26";
+            this.vingtsix.Name = "vingtsix";
+            // 
+            // vingtsept
+            // 
+            this.vingtsept.HeaderText = "27";
+            this.vingtsept.Name = "vingtsept";
+            // 
+            // vingthuit
+            // 
+            this.vingthuit.HeaderText = "28";
+            this.vingthuit.Name = "vingthuit";
+            // 
+            // vingtneuf
+            // 
+            this.vingtneuf.HeaderText = "29";
+            this.vingtneuf.Name = "vingtneuf";
+            // 
+            // trente
+            // 
+            this.trente.HeaderText = "30";
+            this.trente.Name = "trente";
+            // 
+            // trenteun
+            // 
+            this.trenteun.HeaderText = "31";
+            this.trenteun.Name = "trenteun";
+            // 
+            // HolidayForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1082, 578);
+            this.Controls.Add(this.holidays);
+            this.Name = "HolidayForm";
+            this.Text = "HolidayForm";
+            ((System.ComponentModel.ISupportInitialize)(this.holidays)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView holidays;
+        private System.Windows.Forms.DataGridViewTextBoxColumn one;
+        private System.Windows.Forms.DataGridViewTextBoxColumn two;
+        private System.Windows.Forms.DataGridViewTextBoxColumn three;
+        private System.Windows.Forms.DataGridViewTextBoxColumn four;
+        private System.Windows.Forms.DataGridViewTextBoxColumn five;
+        private System.Windows.Forms.DataGridViewTextBoxColumn six;
+        private System.Windows.Forms.DataGridViewTextBoxColumn seven;
+        private System.Windows.Forms.DataGridViewTextBoxColumn eight;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nine;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ten;
+        private System.Windows.Forms.DataGridViewTextBoxColumn onze;
+        private System.Windows.Forms.DataGridViewTextBoxColumn douze;
+        private System.Windows.Forms.DataGridViewTextBoxColumn treize;
+        private System.Windows.Forms.DataGridViewTextBoxColumn quatorze;
+        private System.Windows.Forms.DataGridViewTextBoxColumn quinze;
+        private System.Windows.Forms.DataGridViewTextBoxColumn seize;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dixsept;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dixhuit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dixneuf;
+        private System.Windows.Forms.DataGridViewTextBoxColumn vingt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn vingtun;
+        private System.Windows.Forms.DataGridViewTextBoxColumn vingtdeux;
+        private System.Windows.Forms.DataGridViewTextBoxColumn vingttrois;
+        private System.Windows.Forms.DataGridViewTextBoxColumn vingtquatre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn vingtcinq;
+        private System.Windows.Forms.DataGridViewTextBoxColumn vingtsix;
+        private System.Windows.Forms.DataGridViewTextBoxColumn vingtsept;
+        private System.Windows.Forms.DataGridViewTextBoxColumn vingthuit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn vingtneuf;
+        private System.Windows.Forms.DataGridViewTextBoxColumn trente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn trenteun;
+    }
+}
---
+++ Ouatelse/Ouatelse/Forms/HolidayForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ouatelse.Forms
+{
+    public partial class HolidayForm : Form
+    {
+        public HolidayForm()
+        {
+            InitializeComponent();
+            this.holidays.RowHeadersWidth = 100;
+            foreach (DataGridViewColumn col in this.holidays.Columns)
+            {
+                col.Width = 30;
+                col.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            }
+
+            for (int i = 1; i <= 12; ++i)
+            {
+
+                DataGridViewRow row = new DataGridViewRow();
+                this.holidays.Rows.Add(row);
+                row.HeaderCell.Value = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(i);
+                for (int j = 1; j <= 31; ++j)
+                {
+                    DataGridViewCell cell = new DataGridViewTextBoxCell();
+                    if (j <= DateTime.DaysInMonth(DateTime.Now.Year, i))
+                    {
+
+                        DateTime dateValue = new DateTime(DateTime.Now.Year, i, j);
+                        string day = dateValue.ToString("ddd", new CultureInfo("fr-FR")).Substring(0, 1);
+                        cell.Value = day;
+
+                    }
+                    else
+                    {
+                        cell.ReadOnly = true;
+                        cell.Style.BackColor = Color.Gray;
+                    }
+                }
+            }
+        }
+    }
+}
+
