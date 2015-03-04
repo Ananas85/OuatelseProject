41cbb270d25f283085349f6a3db714485abd2a13
Ananas-Mac <anael.chardan@etu.u-bordeaux.fr>
2015-03-04 19:41:42 +0100

    InitDatabase before we begin UnitTest fastly

--- Ouatelse/Ouatelse/TestDatabase.cs
+++ Ouatelse/Ouatelse/TestDatabase.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SQLite;
+using System.Windows.Forms.VisualStyles;
+using Ouatelse.Models;
 
 namespace Ouatelse
 {
@@ -35,6 +37,7 @@ namespace Ouatelse
             if (!Utils.CheckServer()) return;
             this.connection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
             this.connection.Open();
+            InitDatabase();
         }
         public bool Execute(string query, Dictionary<string, object> parameters = null)
         {
@@ -120,6 +123,130 @@ namespace Ouatelse
         {
             System.IO.File.Delete("MyDatabase.sqlite");
             this.connection.Close();
+        }
+
+        private void InitDatabase()
+        {
+            SQLiteCommand cmd = this.connection.CreateCommand();
+            //Setup the Pragma
+            runningQuery = " PRAGMA foreign_keys = false;";
+            cmd.CommandText = runningQuery;
+            cmd.ExecuteNonQuery();
+
+            //Init Table City
+            runningQuery = City.CreationQuery();
+            cmd.CommandText = runningQuery;
+            cmd.ExecuteNonQuery();
+
+            //Init Table Country
+            runningQuery = Country.CreationQuery();
+            cmd.CommandText = runningQuery;
+            cmd.ExecuteNonQuery();
+
+            //Init Table Customer
+            runningQuery = Customer.CreationQuery();
+            cmd.CommandText = runningQuery;
+            cmd.ExecuteNonQuery();
+
+            //Init Table Employee
+            runningQuery = Employee.CreationQuery();
+            cmd.CommandText = runningQuery;
+            cmd.ExecuteNonQuery();
+
+            //Init Table Gender
+            runningQuery = Gender.CreationQuery();
+            cmd.CommandText = runningQuery;
+            cmd.ExecuteNonQuery();
+
+            //Init Table Holiday
+            runningQuery = Holiday.CreationQuery();
+            cmd.CommandText = runningQuery;
+            cmd.ExecuteNonQuery();
+
+            //Init Table Invoice
+            runningQuery = Invoice.CreationQuery();
+            cmd.CommandText = runningQuery;
+            cmd.ExecuteNonQuery();
+
+            //Init Table InvoiceProduct
+            runningQuery = InvoiceProduct.CreationQuery();
+            cmd.CommandText = runningQuery;
+            cmd.ExecuteNonQuery();
+
+            //Init Table Payment;
+            runningQuery = Payment.CreationQuery();
+            cmd.CommandText = runningQuery;
+            cmd.ExecuteNonQuery();
+
+            //Init Table Product
+            runningQuery = Product.CreationQuery();
+            cmd.CommandText = runningQuery;
+            cmd.ExecuteNonQuery();
+
+            //Init Table Role
+            runningQuery = Role.CreationQuery();
+            cmd.CommandText = runningQuery;
+            cmd.ExecuteNonQuery();
+
+            //Init Table Stock
+            runningQuery = Stock.CreationQuery();
+            cmd.CommandText = runningQuery;
+            cmd.ExecuteNonQuery();
+
+            //Init Table Store
+            runningQuery = Store.CreationQuery();
+            cmd.CommandText = runningQuery;
+            cmd.ExecuteNonQuery();
+
+            //Init the Index City
+            runningQuery = City.CreationIndex();
+            cmd.CommandText = runningQuery;
+            cmd.ExecuteNonQuery();
+
+            //Init the Index Customer
+            runningQuery = Customer.CreationIndex();
+            cmd.CommandText = runningQuery;
+            cmd.ExecuteNonQuery();
+
+            //Init the index Employee
+            runningQuery = Employee.CreationIndex();
+            cmd.CommandText = runningQuery;
+            cmd.ExecuteNonQuery();
+
+            //Init the index Holiday
+            runningQuery = Holiday.CreationIndex();
+            cmd.CommandText = runningQuery;
+            cmd.ExecuteNonQuery();
+
+            //Init the index Invoice
+            runningQuery = Invoice.CreationIndex();
+            cmd.CommandText = runningQuery;
+            cmd.ExecuteNonQuery();
+
+            //Init the index InvoiceProduct
+            runningQuery = InvoiceProduct.CreationIndex();
+            cmd.CommandText = runningQuery;
+            cmd.ExecuteNonQuery();
+
+            //Init the index role
+            runningQuery = Role.CreationIndex();
+            cmd.CommandText = runningQuery;
+            cmd.ExecuteNonQuery();
+
+            //Init the index stock
+            runningQuery = Stock.CreationIndex();
+            cmd.CommandText = runningQuery;
+            cmd.ExecuteNonQuery();
+
+            //Init the index store
+            runningQuery = Store.CreationIndex();
+            cmd.CommandText = runningQuery;
+            cmd.ExecuteNonQuery();
+
+            //Close the pragama
+            runningQuery = " PRAGMA foreign_keys = true;";
+            cmd.CommandText = runningQuery;
+            cmd.ExecuteNonQuery();
         }
 
     }
