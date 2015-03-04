27661664d9fb28cf49a35f0f731be550e1b57bef
Marty Lamoureux <marty@lamoureux-mail.fr>
2015-03-04 19:54:33 +0100

    started sales

--- Ouatelse/Ouatelse/Forms/SalesForm.Designer.cs
+++ Ouatelse/Ouatelse/Forms/SalesForm.Designer.cs
@@ -28,11 +28,232 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.counter = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.editBtn = new System.Windows.Forms.Button();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.items = new System.Windows.Forms.ListView();
+            this.id = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.client = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.date = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.produits = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.prixHT = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.prixTTC = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.searchBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(143, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 40);
+            this.label1.TabIndex = 123;
+            this.label1.Text = "Ventes";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pictureBox1.Image = global::Ouatelse.Properties.Resources.Ouatelse_full_05;
+            this.pictureBox1.Location = new System.Drawing.Point(3, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(147, 91);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 122;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label15
+            // 
+            this.label15.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label15.Location = new System.Drawing.Point(122, -1);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(1017, 90);
+            this.label15.TabIndex = 124;
+            // 
+            // counter
+            // 
+            this.counter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.counter.Location = new System.Drawing.Point(869, 573);
+            this.counter.Name = "counter";
+            this.counter.Size = new System.Drawing.Size(126, 22);
+            this.counter.TabIndex = 131;
+            this.counter.Text = "--";
+            this.counter.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.Image = global::Ouatelse.Properties.Resources.search;
+            this.pictureBox2.Location = new System.Drawing.Point(750, 113);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(32, 31);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 130;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.BackColor = System.Drawing.Color.IndianRed;
+            this.deleteBtn.FlatAppearance.BorderSize = 0;
+            this.deleteBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.deleteBtn.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.deleteBtn.Location = new System.Drawing.Point(488, 106);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(232, 41);
+            this.deleteBtn.TabIndex = 129;
+            this.deleteBtn.Text = "Supprimer une Facture";
+            this.deleteBtn.UseVisualStyleBackColor = false;
+            // 
+            // editBtn
+            // 
+            this.editBtn.BackColor = System.Drawing.Color.DodgerBlue;
+            this.editBtn.FlatAppearance.BorderSize = 0;
+            this.editBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.editBtn.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.editBtn.Location = new System.Drawing.Point(250, 106);
+            this.editBtn.Name = "editBtn";
+            this.editBtn.Size = new System.Drawing.Size(232, 41);
+            this.editBtn.TabIndex = 128;
+            this.editBtn.Text = "Modifier une Facture";
+            this.editBtn.UseVisualStyleBackColor = false;
+            // 
+            // addBtn
+            // 
+            this.addBtn.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.addBtn.FlatAppearance.BorderSize = 0;
+            this.addBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addBtn.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.addBtn.Location = new System.Drawing.Point(12, 106);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(232, 41);
+            this.addBtn.TabIndex = 127;
+            this.addBtn.Text = "Nouvelle Facture";
+            this.addBtn.UseVisualStyleBackColor = false;
+            // 
+            // items
+            // 
+            this.items.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.items.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.id,
+            this.date,
+            this.client,
+            this.produits,
+            this.prixHT,
+            this.prixTTC});
+            this.items.FullRowSelect = true;
+            this.items.GridLines = true;
+            this.items.Location = new System.Drawing.Point(12, 153);
+            this.items.Name = "items";
+            this.items.Size = new System.Drawing.Size(983, 417);
+            this.items.TabIndex = 126;
+            this.items.UseCompatibleStateImageBehavior = false;
+            this.items.View = System.Windows.Forms.View.Details;
+            // 
+            // id
+            // 
+            this.id.Name = "id";
+            this.id.Text = "Référence";
+            this.id.Width = 74;
+            // 
+            // client
+            // 
+            this.client.Name = "prenom";
+            this.client.Text = "Client";
+            this.client.Width = 140;
+            // 
+            // date
+            // 
+            this.date.Name = "adresse";
+            this.date.Text = "Date";
+            this.date.Width = 80;
+            // 
+            // produits
+            // 
+            this.produits.Name = "code_postal";
+            this.produits.Text = "Produits";
+            this.produits.Width = 433;
+            // 
+            // prixHT
+            // 
+            this.prixHT.Name = "villes_id";
+            this.prixHT.Text = "Prix HT";
+            this.prixHT.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.prixHT.Width = 100;
+            // 
+            // prixTTC
+            // 
+            this.prixTTC.Name = "pays";
+            this.prixTTC.Text = "Prix TTC";
+            this.prixTTC.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.prixTTC.Width = 100;
+            // 
+            // searchBox
+            // 
+            this.searchBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchBox.Location = new System.Drawing.Point(788, 115);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(207, 26);
+            this.searchBox.TabIndex = 125;
+            this.searchBox.Visible = false;
+            // 
+            // SalesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1021, 604);
+            this.Controls.Add(this.counter);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.editBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.items);
+            this.Controls.Add(this.searchBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label15);
+            this.Name = "SalesForm";
             this.Text = "SalesForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label counter;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.Button editBtn;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.ListView items;
+        private System.Windows.Forms.ColumnHeader id;
+        private System.Windows.Forms.ColumnHeader date;
+        private System.Windows.Forms.ColumnHeader client;
+        private System.Windows.Forms.ColumnHeader produits;
+        private System.Windows.Forms.ColumnHeader prixHT;
+        private System.Windows.Forms.ColumnHeader prixTTC;
+        private System.Windows.Forms.TextBox searchBox;
     }
 }
--- Ouatelse/Ouatelse/Forms/SalesForm.cs
+++ Ouatelse/Ouatelse/Forms/SalesForm.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Ouatelse.Managers;
+using Ouatelse.Models;
 
 namespace Ouatelse.Forms
 {
@@ -15,6 +17,22 @@ namespace Ouatelse.Forms
         public SalesForm()
         {
             InitializeComponent();
+            Reload();
+        }
+
+        public void Reload()
+        {
+            Invoice[] items = InvoiceManager.Instance.All();
+
+            foreach (Invoice invoice in items)
+            {
+                ListViewItem item = this.items.Items.Add(invoice.Id.ToString());
+                item.SubItems.Add(invoice.Date.ToShortDateString());
+                item.SubItems.Add(invoice.Customer.FullName);
+                item.SubItems.Add(invoice.ProductsString);
+                item.SubItems.Add(invoice.TotalHT.ToString("C"));
+                item.SubItems.Add(invoice.TotalTTC.ToString("C"));
+            }
         }
     }
 }
